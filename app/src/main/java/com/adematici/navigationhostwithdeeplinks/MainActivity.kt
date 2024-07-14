d24c12355b462ef37ecad9aec629b43c6fff3855
AdemAtici <adematicis41@gmail.com>
2024-07-14 13:27:52 +0300

    Add nav host

--- app/src/main/java/com/adematici/navigationhostwithdeeplinks/MainActivity.kt
+++ app/src/main/java/com/adematici/navigationhostwithdeeplinks/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.adematici.navigationhostwithdeeplinks.ui.theme.NavigationHostWithDeeplinksTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +28,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationHostWithDeeplinksTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    NavHostSetup()
                 }
             }
         }
@@ -31,17 +42,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavHostSetup() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController)
+        }
+
+        composable(
+            route = "profile/{userId}",
+            arguments = listOf(navArgument("userId") { type = NavType.StringType }),
+            deepLinks = listOf(navDeepLink { uriPattern = "https://www.example.com/profile/{userId}" })
+        ) { backStackEntry ->
+            ProfileScreen(userId = backStackEntry.arguments?.getString("userId"))
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NavigationHostWithDeeplinksTheme {
-        Greeting("Android")
+fun HomeScreen(navController: NavHostController) {
+    Button(onClick = { navController.navigate("profile/123") }) {
+        Text(text = "Go to Profile")
     }
+}
+
+@Composable
+fun ProfileScreen(userId: String?) {
+    Text(text = "User ID: $userId")
 }
