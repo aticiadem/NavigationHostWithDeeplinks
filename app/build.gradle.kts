6a1b65c3dae93e3020274060f3db0a10166b0e9e
AdemAtici <adematicis41@gmail.com>
2024-07-14 12:35:16 +0300

    Add the navigation library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
